e53d4e0affd22a0b795d8954b24baca0db3438a7
seokwoo-ahn <answ2005@gmail.com>
2022-07-31 03:25:48 +0900

    [feat] add arithmetic operator

---
+++ operator/libs/arithmetic.go
@@ -0,0 +1,21 @@
+package libs
+
+import "fmt"
+
+func Arithmetic() {
+	var x int32 = 7
+	var y int32 = 3
+
+	var s float32 = 3.14
+	var t float32 = 5
+
+	fmt.Println("x + y = ", x+y)
+	fmt.Println("x - y = ", x-y)
+	fmt.Println("x * y = ", x*y)
+	fmt.Println("x / y = ", x/y)
+	fmt.Println("x % y = ", x%y)
+
+	fmt.Println("s * t = ", s*t)
+	fmt.Println("s / t = ", s/t)
+
+}
---
+++ operator/operator.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"golang-tuckers/operator/libs"
+)
+
+func main() {
+	// arithmetic operation test
+	fmt.Println("arithmetic operation test")
+	libs.Arithmetic()
+}
