bda2ba276848a39beb2659dd6d39afe0f77d85bd
seokwoo-ahn <answ2005@gmail.com>
2022-07-31 00:19:56 +0900

    [feat] add float width test

--- fmt/libs/printwidth.go
+++ fmt/libs/printwidth.go
@@ -14,4 +14,12 @@ func PrintWidth() {
 	fmt.Printf("%5d, %5d\n", c, c) // shorter than value
 	fmt.Printf("%05d, %05d\n", c, c)
 	fmt.Printf("%-05d, %-05d\n", c, c)
+
+	var d = 324.13455
+	var e = 3.14
+
+	fmt.Printf("%08.2f\n", d) // width: 8, empty space:0, decimal: 2
+	fmt.Printf("%08.2g\n", d) // width: 8, empty space:0, total num: 2
+	fmt.Printf("%8.5g\n", d)  // width: 8, totla num: 5
+	fmt.Printf("%f\n", e)     // default deciaml: 6
 }
