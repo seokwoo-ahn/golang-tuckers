259c8ce4e326640e7cf8318fa1a040a5786b9af5
seokwoo-ahn <answ2005@gmail.com>
2022-07-31 03:01:05 +0900

    [fix] scan error handling

--- fmt/libs/scan.go
+++ fmt/libs/scan.go
@@ -1,8 +1,14 @@
 package libs
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func Scan() {
+	stdin := bufio.NewReader(os.Stdin)
+
 	var a int
 	var b int
 
@@ -15,11 +21,23 @@ func Scan() {
 	//scan
 	n1, err1 := fmt.Scan(&a, &b)
 
+	if err1 != nil {
+		stdin.ReadString('\n')
+	}
+
 	// scanf
 	n2, err2 := fmt.Scanf("%d %d\n", &c, &d)
 
+	if err2 != nil {
+		stdin.ReadString('\n')
+	}
+
 	// scanln
 	n3, err3 := fmt.Scanln(&e, &f)
+
+	if err3 != nil {
+		stdin.ReadString('\n')
+	}
 
 	fmt.Println(n1, a, b, err1)
 	fmt.Println(n2, c, d, err2)
