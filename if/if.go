04b985d07a84e2bb2b81bf1cd5c478618a900d68
seokwoo-ahn <answ2005@gmail.com>
2022-07-31 13:29:14 +0900

    [feat] if sample

---
+++ if/if.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"golang-tuckers/if/libs"
+)
+
+func main() {
+	// if else
+	fmt.Println("if else sample")
+	libs.Sample(31)
+}
---
+++ if/libs/sample.go
@@ -0,0 +1,14 @@
+package libs
+
+import "fmt"
+
+func Sample(age int) {
+
+	if age >= 10 && age <= 15 {
+		fmt.Println("You are young")
+	} else if age > 30 || age < 20 {
+		fmt.Println("You are not 20s")
+	} else {
+		fmt.Println("Best age of your life")
+	}
+}
