e0b53c3acba8171df826539da413b239eb4eae19
seokwoo-ahn <answ2005@gmail.com>
2022-08-02 22:50:52 +0900

    [feat] string length

---
+++ string/libs/length.go
@@ -0,0 +1,11 @@
+package libs
+
+import "fmt"
+
+func Length() {
+	str1 := "hello world"
+	str2 := "happy day"
+
+	fmt.Println("length of str1 =", len(str1))
+	fmt.Println("length of str2 =", len(str2))
+}
--- string/string.go
+++ string/string.go
@@ -9,4 +9,7 @@ func main() {
 	// string declaration
 	fmt.Println("string declaration")
 	libs.Declaration()
+	// string length
+	fmt.Println("string length")
+	libs.Length()
 }
