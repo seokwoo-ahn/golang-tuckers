94e4c7e30b0784a4ef030355f4b5d0ea830cf28d
seokwoo-ahn <answ2005@gmail.com>
2022-08-06 10:50:58 +0900

    [feat] import package with path

---
+++ package/libs/path.go
@@ -0,0 +1,11 @@
+package libs
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func Path() {
+	fmt.Println("랜덤 숫자 생성")
+	fmt.Println(rand.Int())
+}
---
+++ package/package.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"golang-tuckers/package/libs"
+)
+
+func main() {
+	// import package with path
+	fmt.Println("import package with path")
+	libs.Path()
+}
