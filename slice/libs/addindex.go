738743133e2d7dd0ab58a5af9c9860d24b267e4e
seokwoo-ahn <answ2005@gmail.com>
2022-08-11 11:20:42 +0900

    [feat] add slice with copy

--- slice/libs/addindex.go
+++ slice/libs/addindex.go
@@ -3,17 +3,25 @@ package libs
 import "fmt"
 
 func AddIndex() {
-	slice := []int{1, 2, 3, 4, 5, 6}
+	slice1 := []int{1, 2, 3, 4, 5, 6}
 
-	slice = append(slice, 0)
+	slice1 = append(slice1, 0)
 
 	idx := 2
 
-	for i := len(slice) - 2; i >= idx; i-- {
-		slice[i+1] = slice[i]
+	for i := len(slice1) - 2; i >= idx; i-- {
+		slice1[i+1] = slice1[i]
 	}
 
-	slice[idx] = 100
+	slice1[idx] = 100
 
-	fmt.Println(slice)
+	fmt.Println(slice1)
+
+	slice2 := []int{7, 8, 9, 10, 11}
+
+	slice2 = append(slice2, 0)
+	copy(slice2[idx+1:], slice2[idx:])
+	slice2[idx] = 100
+
+	fmt.Println(slice2)
 }
