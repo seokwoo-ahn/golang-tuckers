822a5ce1627b02a017e35fa8dd70ce589e8d8f2d
seokwoo-ahn <answ2005@gmail.com>
2022-08-20 17:55:03 +0900

    [feat] queue

--- datastructure/datastructure.go
+++ datastructure/datastructure.go
@@ -9,4 +9,11 @@ func main() {
 	// list
 	fmt.Println("list")
 	libs.List()
+	// queue
+	fmt.Println("queue")
+	queue := libs.NewQueue()
+	for i := 1; i < 5; i++ {
+		queue.Push(i)
+	}
+	fmt.Println(queue.Pop())
 }
