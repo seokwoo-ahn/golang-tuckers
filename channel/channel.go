093d838b86c641a1ac900d69068c5323b5b2d8ec
seokwoo-ahn <answ2005@gmail.com>
2022-08-27 23:24:36 +0900

    [feat] channel

---
+++ channel/channel.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"golang-tuckers/channel/libs"
+)
+
+func main() {
+	// data channel
+	fmt.Println("channel gen")
+	libs.Data()
+}
---
+++ channel/libs/data.go
@@ -0,0 +1,24 @@
+package libs
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func Data() {
+	var wg sync.WaitGroup
+	ch := make(chan int)
+
+	wg.Add(1)
+	go square(&wg, ch)
+	ch <- 9
+	wg.Wait()
+}
+
+func square(wg *sync.WaitGroup, ch chan int) {
+	n := <-ch
+	time.Sleep(time.Second)
+	fmt.Printf("Square: %d\n", n*n)
+	wg.Done()
+}
